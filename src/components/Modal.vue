2fdc39af154a709cf18cc7bfa221b0b2ef5b5a94
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2022-08-02 10:48:52 +0300

    Added dark mode to modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -24,7 +24,7 @@ div {
   transform: translate(-50%, -50%);
   padding: 20px;
   border-radius: 20px;
-  box-shadow: 0px 0px 8px -4px #000000;
-  background-color: white;
+  box-shadow: 0px 0px 8px -4px var(--text-color);
+  background-color: var(--background-color);
 }
 </style>
